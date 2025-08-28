e54225118b4a31cfcaa507eee022212cf3a979f5
Shadab <sadabansari451@gmail.com>
2025-08-28 22:55:47 +0530

    donee

--- frontend/components/ClientProviders.js
+++ frontend/components/ClientProviders.js
@@ -1,38 +1,29 @@
-// "use client";
-
-// import { useState, useEffect, useRef } from "react";
-// import { usePathname } from "next/navigation";
-// import { SocketProvider } from "@/context/SocketContext";
-// import SkeletonLoader from "@/components/SkeletonLoader";
-
-// export default function ClientProviders({ children }) {
-//   const [loading, setLoading] = useState(false);
-//   const pathname = usePathname();
-//   const firstLoad = useRef(true); // track if it's the very first render
-
-//   useEffect(() => {
-//     if (!pathname) return;
-
-//     if (firstLoad.current) {
-//       // Skip loader on first mount
-//       firstLoad.current = false;
-//       return;
-//     }
-
-//     // Show loader only on route changes after first load
-//     setLoading(true);
-//     const timeout = setTimeout(() => setLoading(false), 1000);
-//     return () => clearTimeout(timeout);
-//   }, [pathname]);
-
-//   return (
-//     <SocketProvider>{loading ? <SkeletonLoader /> : children}</SocketProvider>
-//   );
-// }
 "use client";
 
+import { useState, useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 import { SocketProvider } from "@/context/SocketContext";
+import SkeletonLoader from "@/components/SkeletonLoader";
 
 export default function ClientProviders({ children }) {
-  return <SocketProvider>{children}</SocketProvider>;
+  const [loading, setLoading] = useState(false);
+  const pathname = usePathname();
+  const firstLoad = useRef(true); // track if it's the very first render
+
+  useEffect(() => {
+    if (!pathname) return;
+
+    if (firstLoad.current) {
+      // Skip loader on first mount
+      firstLoad.current = false;
+      return;
+    }
+    setLoading(true);
+    const timeout = setTimeout(() => setLoading(false), 1000);
+    return () => clearTimeout(timeout);
+  }, [pathname]);
+
+  return (
+    <SocketProvider>{loading ? <SkeletonLoader /> : children}</SocketProvider>
+  );
 }
