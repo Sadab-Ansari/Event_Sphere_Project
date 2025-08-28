70ede453508477d898d2314432328c827761845b
Shadab <sadabansari451@gmail.com>
2025-08-28 23:53:48 +0530

    doneeeeee

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,18 +1,22 @@
 // import Navbar from "@/components/Navbar";
 // import Footer from "@/components/Footer";
 import "../app/globals.css";
-import ClientProviders from "@/components/ClientProviders"; // Use ClientProviders instead of SocketProvider
+import ClientProviders from "@/components/ClientProviders";
 import { Suspense } from "react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Event Sphere</title>
+      </head>
       <body className="flex flex-col min-h-screen">
         <Suspense fallback={null}>
           <ClientProviders>
             {/* <Navbar /> */}
-            <main className="flex-1">{children}</main> {/* Page content */}
-            {/* <Footer /> */} {/* Uncomment if needed */}
+            <main className="flex-1">{children}</main>
+            {/* <Footer /> */}
           </ClientProviders>
         </Suspense>
       </body>
